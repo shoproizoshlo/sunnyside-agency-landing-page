512ab91faa69a6c522f8d62d0741e07549b93347
shoproizoshlo <iamsusunny@gmail.com>
2023-10-08 16:16:33 +0200

    fixed header position

---
+++ src/script.js
@@ -0,0 +1,20 @@
+// function teste() {
+//   if (document.getElementById("menu").style.display == "block") {
+//     document.getElementById("menu").style.display = "none";
+//   } else {
+//     document.getElementById("menu").style.display = "block";
+//   }
+// }
+
+// document.getElementById("menu").addEventListener("click", teste);
+
+// function lineChange() {
+//   hrs = document.querySelectorAll("hr");
+//   hrs.forEach((hr) => (hr.style.opacity = 1));
+// }
+
+// let buttons = document.querySelectorAll("service-text button");
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", lineChange);
+// });
