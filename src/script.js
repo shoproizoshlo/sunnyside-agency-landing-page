30976e67b6f91d5965e59df248a0c9cb1cd2b384
shoproizoshlo <iamsusunny@gmail.com>
2023-10-08 16:42:26 +0200

    fixed hover button

--- src/script.js
+++ src/script.js
@@ -1,19 +1,23 @@
-// function teste() {
-//   if (document.getElementById("menu").style.display == "block") {
-//     document.getElementById("menu").style.display = "none";
-//   } else {
-//     document.getElementById("menu").style.display = "block";
-//   }
-// }
+function teste() {
+  if (document.getElementById("menu").style.display == "block") {
+    document.getElementById("menu").style.display = "none";
+  } else {
+    document.getElementById("menu").style.display = "block";
+  }
+}
 
 // document.getElementById("menu").addEventListener("click", teste);
 
 // function lineChange() {
-//   hrs = document.querySelectorAll("hr");
-//   hrs.forEach((hr) => (hr.style.opacity = 1));
+//   console.log("click bottun");
+//   hrs = document.querySelectorAll(".service-text hr");
+//   hrs.forEach((hr) => {
+//     console.log("click hr");
+//     hr.style.opacity = "0.9";
+//   });
 // }
 
-// let buttons = document.querySelectorAll("service-text button");
+// let buttons = document.querySelectorAll(".service-text button");
 
 // buttons.forEach((button) => {
 //   button.addEventListener("click", lineChange);
