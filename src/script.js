1b21b8228aa4ac0f35226f70db5309dc887df760
shoproizoshlo <iamsusunny@gmail.com>
2023-10-11 19:53:42 +0200

    fixed design/photography photo posistion

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 function teste() {
   if (document.getElementById("menu").style.display == "block") {
     document.getElementById("menu").style.display = "none";
+    document.getElementById("headline-text").style.display = "block";
   } else {
     document.getElementById("menu").style.display = "block";
+    document.getElementById("headline-text").style.display = "none";
   }
 }
 
