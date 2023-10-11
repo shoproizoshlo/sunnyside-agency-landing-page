0c0891646f1982133d737336641cb91f71a4ba02
shoproizoshlo <iamsusunny@gmail.com>
2023-10-11 20:53:30 +0200

    cleaned some code

--- src/script.js
+++ src/script.js
@@ -7,20 +7,3 @@ function teste() {
     document.getElementById("headline-text").style.display = "none";
   }
 }
-
-// document.getElementById("menu").addEventListener("click", teste);
-
-// function lineChange() {
-//   console.log("click bottun");
-//   hrs = document.querySelectorAll(".service-text hr");
-//   hrs.forEach((hr) => {
-//     console.log("click hr");
-//     hr.style.opacity = "0.9";
-//   });
-// }
-
-// let buttons = document.querySelectorAll(".service-text button");
-
-// buttons.forEach((button) => {
-//   button.addEventListener("click", lineChange);
-// });
